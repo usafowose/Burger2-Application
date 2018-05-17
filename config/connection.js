6414fbeb993dddd394c6fc920d04c62ca1105b1b
Geoff's Macbook <geoffsmacbook@lawn-143-215-126-8.lawn.gatech.edu>
2018-05-17 19:36:48 -0400

    Updated logic across MVC

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'burgers_db'
+});
+
+connection.connect((err) => {
+  if (err) return console.error(`error connecting: ${err.stack}`);
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+('Bacon Blue Cheese', false),
+('Turkey Avocado', false),
+('Mushroom Swiss', false);
