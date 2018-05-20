380117cac98b828a1f14ec1d250680786c5f8d0c
Geoff's Macbook <geoffsmacbook@Geoffss-MBP.attlocal.net>
2018-05-20 18:54:12 -0400

    added JAWSDB to help with deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'burgers_db'
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  });
+}
 
 connection.connect((err) => {
   if (err) return console.error(`error connecting: ${err.stack}`);
